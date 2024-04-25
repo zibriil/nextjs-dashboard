a90fdae5ac271c110ac96ae4a04cdde9b66c0450
Em Izrofi <dev.zibril101@gmail.com>
2024-04-25 15:49:05 +0700

    fix

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import bcrypt from 'bcrypt';
+import { User } from './app/lib/definitions';
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
